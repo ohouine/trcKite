e859704bb8e8ddd51f8b4e009dd78aae9794b60b
DAYAN-MDX <dayan.mdx@eduge.ch>
2024-08-21 11:37:37 +0200

    ebauche page d'acceuil

---
+++ script/script.js
@@ -0,0 +1,60 @@
+const formWeight = document.querySelector("#formWeight");
+
+formWeight.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const weight = formWeight.querySelector("#weight").value
+    let tips = "weight invalid"
+    if (weight > 0 && weight <= 60) {
+        tips = "you need a 9m2 kite"
+    }else if (weight > 60 && weight <= 65) {
+        tips = "you need a 9-10m2 kite"
+    }else  if (weight > 65 && weight <= 70) {
+        tips = "you need a 10-11m2 kite"
+    } else  if (weight > 70 && weight <= 75) {
+        tips = "you need a 12m2 kite"
+    }else  if (weight > 75 && weight <= 80) {
+        tips = "you need a 13m2 kite"
+    }else  if (weight > 80 && weight <= 90) {
+        tips = "you need a 14m2 kite"
+    } else  if (weight > 90) {
+        tips = "you need a 15m2 kite"
+    }
+    document.querySelector("#infoKitsWidth").textContent = tips
+
+    const windSpeed = await getMeteo()
+    document.querySelector("#windInfo").textContent = `wind is going at ${windSpeed.value} km/h with an angle of ${windSpeed.wind_direction}`
+
+})
+
+async function getImage(){
+    let init = {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+		    'Accept-Version': 'v5'
+        },
+    }
+
+    let response = await fetch("https://api.waifu.im/search?f", init)
+
+    let jsonResponse = await response.json()
+
+    return jsonResponse.images[0].url;
+}
+
+setInterval(async () => {document.querySelector("#waifu").src = await getImage();},5000);
+
+async function getMeteo() {
+    let init = {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+
+    let response = await fetch("https://data.geo.admin.ch/ch.meteoschweiz.messwerte-windgeschwindigkeit-kmh-10min/ch.meteoschweiz.messwerte-windgeschwindigkeit-kmh-10min_en.json", init)
+    let jsonResponse = await response.json()
+
+    return jsonResponse.features[51].properties
+}
